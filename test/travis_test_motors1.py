db6025586814ce8cf4522bce04039f8981875106
seeabird <seeabird747@gmail.com>
2020-01-29 06:12:33 +0000

    mod travis_test_motors1.py

--- test/travis_test_motors1.py
+++ test/travis_test_motors1.py
@@ -14,19 +14,20 @@ class MotorTest(unittest.TestCase):
 	def test_node_exist(self):
 		nodes = rosnode.get_node_names()
 		self.assertIn('/motors', nodes, "node does not exitst")
-	'''
+	
 	def test_put_freq(self):
 		pub = rospy.Publisher('/motor_raw', MotorFreqs)
 		m = MotorFreqs()
-		m.left_hz = 123
-		m.right_hz = 456
+		m.left_hz = 500
+		m.right_hz = 500
 		for i in range(10):
 			pub.publish(m)
 			time.sleep(0.1)
+			print 'pub.publish(m)'
 
 		self.file_check("rtmotor_raw_l0", m.left_hz, "wrong left value from motor_raw")
 		self.file_check("rtmotor_raw_r0", m.right_hz, "wrong right value from motor_raw")
-	'''
+	
 	def test_put_cmd_vel(self):
 		pub = rospy.Publisher('/cmd_vel', Twist)
 		m = Twist()
