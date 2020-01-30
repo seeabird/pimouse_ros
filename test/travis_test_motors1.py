6754c46dffa28985f2b145dc148cbd2f9ceb1b33
seeabird <seeabird747@gmail.com>
2020-01-30 00:08:52 +0000

    mod travis_test_motors1.py

--- test/travis_test_motors1.py
+++ test/travis_test_motors1.py
@@ -5,6 +5,7 @@ import rosnode, rospy
 import time
 from pimouse_ros.msg import MotorFreqs
 from geometry_msgs.msg import Twist
+from std_srvs.srv import Trigger
 
 class MotorTest(unittest.TestCase):
 	def setUp(self):
